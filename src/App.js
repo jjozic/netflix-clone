4f387ccd4b4426f992a5b32bf8c9b025983000ea
jjozic <jjozic98@gmail.com>
2020-08-09 18:40:59 +0200

    Added jumbotron and images to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import Home from './pages/home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Netflix Clone</h1>
-    </div>
-  );
+    <Router>
+        <Switch>
+            <Route path="/signin">
+              <p>I will be the sign in page</p>
+            </Route>
+            <Route path="/signup">
+              <p>I will be the sign up page</p>
+            </Route>
+            <Route path="/browse">
+              <p>I will be the browse page</p>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+        </Switch>
+    </Router>
+  )
 }
+
 
 export default App;
